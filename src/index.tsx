f25c1ff08d9e7e67452f4f2b08d9330da2a1316c
y-yanagii <yyanagisawa555@gmail.com>
2021-07-13 00:09:29 +0900

    エディタ画面の見た目を作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import { Editor } from './pages/editor';
 
-const Header = styled.h1
-  `color: red;`
+// styled.HTMLタグ名で生成したいHTMLタグを指定してその後続く``内にCSSを記述すると、そのコンポーネントにスタイルが適用される
+// styled-componentsのcreateGlobalStyle使ってページ全体に適用できるスタイル
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
+`
 
-const Main = (<Header>Markdown Editor</Header>)
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+)
 
-render(Main, document.getElementById('app'))
+render(Main, document.getElementById('app'));
