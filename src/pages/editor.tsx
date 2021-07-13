fe19adc95aea09bd2bad39bfded27311ef0b33e1
y-yanagii <yyanagisawa555@gmail.com>
2021-07-13 12:55:02 +0900

    react-markdownをnpmインストールしてリアルタイムにマークダウン形式に変換

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { useStateWithStorage } from '../hooks/use_state_with_storage';
+import * as ReactMarkdown from 'react-markdown';
 
 // useState関数をReactから取り出す
 // const { useState } = React; // import { useState } from 'react'と同等
@@ -65,7 +66,11 @@ export const Editor: React.FC = () => {
           onChange={(event) => setText(event.target.value)}
           value={text}
         />
-        <Preview>プレビューエリア</Preview>
+        <Preview>
+          <ReactMarkdown>
+            {text}
+          </ReactMarkdown>
+        </Preview>
       </Wrapper>
     </>
   )
